ab0a35f9250884e6e5cdc1a3fe882218918e8566
EpicStuff <EpicStuff@users.noreply.github.com>
2023-05-11 13:43:56 -0400

    switching over to nicegui

--- tmp2.py
+++ tmp2.py
@@ -1,29 +1,8 @@
-import webbrowser
-import PySimpleGUIWeb as sg
+from nicegui import ui
 
-urls = {
-    'Google':'https://www.google.com',
-    'Amazon':'https://www.amazon.com/',
-    'NASA'  :'https://www.nasa.gov/',
-    'Python':'https://www.python.org/',
-}
+ui.tree([
+    {'id': 'numbers', 'children': [{'id': '1'}, {'id': '2'}]},
+    {'id': 'letters', 'children': [{'id': 'A'}, {'id': 'B'}]},
+], label_key='id', on_select=lambda e: ui.notify(e.value))
 
-items = sorted(urls.keys())
-
-sg.theme("DarkBlue")
-font = ('Courier New', 16, 'underline')
-
-layout = [[sg.Text(txt, tooltip=urls[txt], enable_events=True, font=font,
-    key=f'URL {urls[txt]}')] for txt in items]
-window = sg.Window('Hyperlink', layout, size=(250, 150), finalize=True)
-
-while True:
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
-        break
-    elif event.startswith("URL "):
-        url = event.split(' ')[1]
-        webbrowser.open(url)
-    print(event, values)
-
-window.close()
+ui.run()

84bad9a76d27ce9145d65cea8aabae821f980e1b
EpicStuff <EpicStuff@users.noreply.github.com>
2023-06-05 19:34:58 -0400

    implemented "jail breaking" nicegui

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
 from nicegui import ui
-import asyncio
+import nicegui
+from pathlib import Path
 
-ui.label('Hello World')
+# x = nicegui.dependencies.js_components
+# y = x['aggrid']
+# z = nicegui.dependencies.js_dependencies
+w = nicegui.dependencies.js_dependencies[0]
+assert w.dependents == {'aggrid'}, 'Overwriting NiceGUI aggrid failed'
+w.path = Path('ag-grid-enterprise.min.js')
 
-async def test():
-    ui.run()
+columnDefs = [{'headerName': 'Name', 'field': 'name', 'rowGroup': True, 'hide': True},]
+rowData = [
+	{'link': 1},
+	{'link': 2},
+	{'link': 3},
+]
+gridOptions = {
+	'defaultColDef': {
+		'resizable': True,
+		'suppressMenu': True,
+		'cellRendererParams': {'suppressCount': True, },
+	},
+	'autoGroupColumnDef': {
+		'headerName': 'Name',
+		'field': 'link',
+	},
+	'columnDefs': columnDefs,
+  		'rowData': rowData,
+  		'rowHeight': 32,
+  		'animateRows': True,
+  		'suppressAggFuncInHeader': True,
+}
+ui.aggrid(gridOptions, theme='alpine-dark').style('height: calc(100vh - 164px)')
 
-
-asyncio.run(test())
+ui.run()

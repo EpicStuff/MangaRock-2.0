176efc26ea37c45b213dddb5ce910cd552b36344
EpicStuff <EpicStuff@users.noreply.github.com>
2023-10-06 14:38:18 -0400

    test

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
-import asyncio
+import asyncio, sys
 from nicegui import ui, app
 
-async def tmp():
-	await ui.run_javascript('window.addEventListener("load", () =>)', respond=False)
 
-def tmp2(e):
-	print('tmp2')
+class Log():
+	def __init__(self, log):
+		self.stdout = sys.stdout
+		self.log = log
+	def write(self, s):
+		self.log.push(s)
+		# self.log.write(s)
+	def flush(self):
+		pass
+	def isatty(self):
+		return False
 
 
-ui.button('test', on_click=tmp).on('load', tmp2)
+ui.button('test', on_click=lambda: print('test'))
 
-app.on_connect(tmp2)
-
+sys.stdout = tmp(ui.log())
+# sys.stdout = tmp(open('test.txt', 'a'))
 ui.run(dark=True)
